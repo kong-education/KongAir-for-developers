249b05a3588ec6d98b298df02a821744df0323e8
Duncan <duncan.deane@konghq.com>
2024-06-01 17:48:33 +0100

    added test

---
+++ flight-data/flights/tests/int-test-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Send the request and capture the headers
+response=$(http --headers --check-status GET http://localhost/flights)
+status_code=$(echo "$response" | grep HTTP | awk '{print $2}')
+provider_header=$(echo "$response" | grep x-flights-provider | awk '{print $2}')
+
+# Check the response
+if [ "$status_code" -eq 200 ] && [ "$provider_header" == "kong-air" ]; then
+  echo "Test passed: Status code is 200 and x-flights-provider is kong-air"
+  exit 0
+else
+  echo "Test failed"
+  echo "Status code: $status_code"
+  echo "x-flights-provider: $provider_header"
+  exit 1
+fi
