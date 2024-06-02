84c63e78191070a874087c58345d21b9d7b3a232
Duncan <duncan.deane@konghq.com>
2024-06-02 13:40:28 +0100

    added check

--- .github/flights-gitflow.sh
+++ .github/flights-gitflow.sh
@@ -2,6 +2,10 @@
 
 cd /home/ubuntu/KDIL-201
 
+if [ -f /home/ubuntu/KDIL-201/my-kongair ]; then
+  rm -rf /home/ubuntu/KDIL-201/my-kongair
+fi
+
 git clone https://github.com/duncdeane/my-kongair.git
 
 cd /home/ubuntu/KDIL-201/my-kongair
