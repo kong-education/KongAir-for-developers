925046699617d991dd3e753048f25f248951a038
Duncan <duncan.deane@konghq.com>
2024-06-02 20:13:15 +0100

    int test script

--- flight-data/flights/tests/int-test-1.sh
+++ flight-data/flights/tests/int-test-1.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-sleep 5
-# Send the request and capture the headers
-response=$(http --headers --check-status GET http://localhost:8000/flights)
-status_code=$(echo "$response" | grep HTTP | awk '{print $2}')
-provider_header=$(echo "$response" | grep x-flights-provider | awk '{print $2}')
 
-# Check the response
-if [ "$status_code" -eq 200 ] && [ "$provider_header" == "kong-air" ]; then
-  echo "Test passed: Status code is 200 and x-flights-provider is kong-air"
-  exit 0
+# URL to make the GET request
+
+# Make the GET request using httpie and capture the output
+response=$(http --headers GET http://localhost:8000/flights)
+
+# Check if the status code is 200
+status_code=$(echo "$response" | grep "HTTP/1.1 200 OK")
+
+# Check if the x-flights-provider header is kong-air
+provider_header=$(echo "$response" | grep "x-flights-provider: kong-air")
+
+# Verify both conditions
+if [[ -n "$status_code" && -n "$provider_header" ]]; then
+    echo "Success: Status code is 200 and x-flights-provider is kong-air."
 else
-  echo "Test failed"
-  echo "Status code: $status_code"
-  echo "x-flights-provider: $provider_header"
-  exit 1
+    echo "Failure: Either the status code is not 200 or x-flights-provider is not kong-air."
 fi
